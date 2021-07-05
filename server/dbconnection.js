d3ae4647847a7b383e545cd03b54544b1486b0fd
RickyJRO <ricardojribeirooliveira@gmail.com>
2021-07-05 03:01:59 +0100

    letsgo

--- server/dbconnection.js
+++ server/dbconnection.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/${process.env.DATABASE}`
 
-const dba = new Client({host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASS, database: process.env.DATABASE});
 
 const proConfig = process.env.DATABASE_URL
 
