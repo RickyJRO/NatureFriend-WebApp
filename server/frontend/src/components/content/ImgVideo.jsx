b01150dca75b383579ed5d16da2c347fd795d541
RickyJRO <ricardojribeirooliveira@gmail.com>
2021-07-07 01:06:59 +0100

    mota

--- server/frontend/src/components/content/ImgVideo.jsx
+++ server/frontend/src/components/content/ImgVideo.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function ImgVideo(props) {
+    const [foto, setFoto] = useState(null);
+
+    var array = props.img.split(".")
+    console.log(isNaN(parseInt(array[0])));
+
+    if(isNaN(parseInt(array[0])) == true){
+        setFoto(props.img)
+    }
+
     return (
         <div>
             <div>
             {props.format == "mp4" ? <video src={'/' + props.img} autoPlay muted loop></video> 
-            : <img className='UsersPostsImg' src={'/' + props.img} />}
+            : <img className='UsersPostsImg' src={foto != null ?  '/' + props.img : 'https://naturefriend-mobile.herokuapp.com/' + props.img} />}
             </div>
         </div>
     )
